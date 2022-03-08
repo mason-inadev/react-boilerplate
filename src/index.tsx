2d80e2d66f47217729c72003a50d991112960d6d
Mason Donley <mason.donley@inadev.com>
2022-03-08 10:47:47 -0500

    removed unneeded files

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import GlobalStyle from './styles/global'
+import Router from './Router'
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+        <>
+    <GlobalStyle />
+    <Router/>
+    </>
   </React.StrictMode>,
   document.getElementById('root')
 );
