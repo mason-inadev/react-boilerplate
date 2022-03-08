378f7ac5fa802e6477108b78e1c348d919a6b9c2
Mason Donley <mason.donley@inadev.com>
2022-03-08 09:38:13 -0500

    created global style

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Router  from './Router';
+import GlobalStyle from "./styles/global"
 
 function App() {
   return (
+    <>
+    <GlobalStyle />
     <Router/>
+    </>
   );
 }
 
---
+++ src/styles/global.tsx
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+:root {}
+
+body {}
+`
+
+export default GlobalStyle
